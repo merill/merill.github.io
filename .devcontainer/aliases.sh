7836f08c25e1014fbde490fed9db681a36be2e4a
Sean Killeen <SeanKilleen@gmail.com>
2024-11-12 22:35:00 -0500

    Add devcontainer

---
+++ .devcontainer/aliases.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# We get the first workspace, because there should only be one
+# This is so that when someone updates their devcontainer name, it doesn't get screwed up
+first_workspace="$(cd /workspaces && ls | head -1)"
+alias gotoworkspace="cd /workspaces/$first_workspace"
+alias localdev="gotoworkspace && bundle exec jekyll serve --future --force_polling --livereload"
+alias build="gotoworkspace && bundle exec jekyll build"
+alias serve="gotoworkspace && bundle exec jekyll serve --future --force_polling --livereload"
+alias spellcheck="gotoworkspace && cspell --no-progress --config cSpell.json **/*.md"
+alias lint="gotoworkspace && markdownlint-cli2 --config .markdownlint-cli2.jsonc **/*.md"
+alias lintfix="gotoworkspace && markdownlint-cli2 --fix --config .markdownlint-cli2.jsonc **/*.md"
